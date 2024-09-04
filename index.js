9b3426d887d86c84cda743bb4380e83be3ebb36a
FDC-gabriel <125229674+FDC-gabriel@users.noreply.github.com>
2024-09-04 16:37:57 +0800

    portfolio update [added text randomizer]

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const terminalOptions = [
     {name: '404', key: '404'}
 ]
 
-// let notFoundCounter = 0
+let notFoundCounter = 0
 
-const namespace = encodeURIComponent('portfolio-gabrielcorpuz')
+const namespace = encodeURIComponent('portfolio-gabrielcorpuz-new')
 const keyspace = encodeURIComponent('visits')
 
 const getVisits = async () => {
@@ -34,7 +34,51 @@ const downloadResume = () => {
     link.href = 'Resume.pdf'
     link.download = 'resume-gabriel-corpuz'
     link.click()
-    link.remove()
+}
+
+const downloadLogs = async () => {
+    const response = await fetch(`https://api.counterapi.dev/v1/${namespace}/${keyspace}/list?group_by=day`)
+    const data = await response.json()
+    
+    const link = document.createElement('a')
+    link.href = URL.createObjectURL(new Blob([JSON.stringify(data, null, 2)], {type: 'application/json'}))
+    link.download = 'logs.json'
+    link.click()
+}
+
+function RandomizedText({text}){
+    const [displayText, setDisplayText] = useState(text)
+
+    if(!isReduceMotion){
+        useEffect(() => {
+            const speed = 50
+            const baseString = '1234567890 ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz .,!@#$%&'
+    
+            const skip = 20
+            const charJump = Math.floor(Math.abs(text.length / skip))
+            
+            let iteration = 0
+    
+            function shuffleText(){
+                if(iteration <= text.length){
+                    const randomChar = baseString.charAt(Math.floor(Math.random() * baseString.length))
+                    const newText = text.slice(0, iteration) + randomChar + text.slice(iteration + skip)
+    
+                    setDisplayText(newText)
+                    iteration += charJump || text.length / 3
+    
+                    setTimeout(shuffleText, speed)
+                }
+                else{
+                    setDisplayText(text)
+                }
+            }
+    
+            shuffleText()
+        }, [text])
+    }
+
+    return <>{displayText}</>
 }
 
 function TerminalHistory({currstate, visits}) {
@@ -47,8 +91,12 @@ function TerminalSection({currState, stateSetter}) {
             case 'home':
                 return (
                     <>
-                    <p>Hi! Welcome to my portfolio.</p>
-                    <p>To interact with the terminal, input 'cd ../' followed by the <span>(option)</span>.</p>
+                    <p><RandomizedText text="Hi! Welcome to my portfolio." /></p>
+                    <p>
+                        <RandomizedText text="To interact with the terminal, input 'cd ../' followed by the " />
+                        <span className="option"><RandomizedText text="(option)"/></span>
+                        <RandomizedText text="."/>
+                    </p>
                     </>
                 )
             case 'about':
@@ -56,49 +104,57 @@ function TerminalSection({currState, stateSetter}) {
                     <div className="col2">
                         <div className="pp">
                             <img src="./res/images/profile.jpg" alt="Gabriel D. Corpuz" />
-                            <p>Hello! I'm Gabriel Corpuz, a 26-year-old front-end web developer based in Cebu City. I'm passionate about creating dynamic and responsive websites, and I'm currently on a journey to become a full-stack developer.</p>
+                            <p><RandomizedText text="Hello! I'm Gabriel Corpuz, a 26-year-old front-end web developer based in Cebu City. I'm passionate about creating dynamic and responsive websites, and I'm currently on a journey to become a full-stack developer." /></p>
                         </div>
                         <div className="edu">
-                            <h2>Education:</h2>
-                            <p>Saint Columban College - Bachelor of Science in Information Technology</p>
+                            <h2><RandomizedText text="Education:" /></h2>
+                            <p><RandomizedText text="Saint Columban College - Bachelor of Science in Information Technology" /></p>
                         </div>
                         <div className="interest">
-                            <h2>Interests:</h2>
-                            <p>CSS/JS updates, AI, other advancements in technology</p>
+                            <h2><RandomizedText text="Interests:" /></h2>
+                            <p><RandomizedText text="CSS/JS updates, AI, other advancements in technology" /></p>
                         </div>
                         <div className="hobby">
-                            <h2>Hobbies:</h2>
-                            <p>Reading books/ebooks, Playing guitar, travelling, working out, playing video games</p>
+                            <h2><RandomizedText text="Hobbies:"/></h2>
+                            <p><RandomizedText text="Reading books/ebooks, Playing guitar, traveling, working out, playing video games"/></p>
                         </div>
                     </div>
                 )
             case 'exp':
                 return (
                     <>
-                    <h2>Experience:</h2>
-                    <p>Proweaver, Inc. - 3 years (Web Developer)</p>
-                    <p>Forty Degrees Celsius, Inc. - Current (Front End Developer)</p>
+                    <h2><RandomizedText text="Experience:" /></h2>
+                    <p><RandomizedText text="4+ years: Front End Developer" /></p>
                     <br/>
-                    <h2>Skills:</h2>
-                    <p>JQuery, React, NodeJS, Express, PHP, Laravel, Wordpress</p>
+                    <h2><RandomizedText text="Toolset:"/></h2>
+                    <p><RandomizedText text="JQuery, React, NodeJS, Express, PHP, Laravel, Wordpress"/></p>
                     </>
                 )
             case 'resume':
                 downloadResume()
-                stateSetter('home')
+                setTimeout(() => stateSetter('home'), 0)
             case 'contact':
                 return (
                     <>
-                    <h2>Contact: </h2>
-                    <p>Cellphone: <a href="tel:+639305933195">+63 930 593 3195</a></p>
-                    <p>Email: <a href="mailto:gabrielcorpuz0914@gmail.com">gabrielcorpuz0914@gmail.com</a></p>
+                    <h2><RandomizedText text="Contact:"/></h2>
+                    <p>
+                        <RandomizedText text="Cellphone:"/>&nbsp;
+                        <a href="tel:+639305933195"><RandomizedText text="+63 930 593 3195"/></a>
+                    </p>
+                    <p>
+                        <RandomizedText text="Email:"/>&nbsp;
+                        <a href="mailto:gabrielcorpuz0914@gmail.com"><RandomizedText text="gabrielcorpuz0914@gmail.com"/></a>
+                    </p>
                     </>
                 )
+            case 'log':
+                downloadLogs()
+                setTimeout(() => stateSetter('home'), 0)
             default:
                 stateSetter('404')
                 return (
                     <>
-                    <p>Page not found (ᴗ_ ᴗ。)</p>
+                    <p><RandomizedText text="Page not found (ᴗ_ ᴗ。)"/></p>
                     </>
                 )
                 
@@ -114,8 +170,9 @@ function TerminalOptions({optionMap, currState}) {
         {optionMap.map((opt) => (
             opt.key !== currState &&
             opt.key !== '404' && (
-                <li key={opt.key}>
-                    {opt.name} <small>({opt.key})</small>
+                <li key={`${currState}-${opt.key}`}>
+                    <RandomizedText text={opt.name}/>&nbsp;
+                    <small><RandomizedText text={`(${opt.key})`}/></small>
                 </li>
             )
         ))}
